a591fc5ace460dde91b6e653bc21066e243dd034
theedrifter <tangoat2019@users.noreply.github.com>
2020-05-01 00:19:02 -0500

    Min Swap Palindrome and Min move

--- Interview Prep/Microsoft/min_move.py
+++ Interview Prep/Microsoft/min_move.py
@@ -1,17 +1,23 @@
 # https://leetcode.com/discuss/interview-question/398026/
 
 def main():
-	print(min_move('baaaaa'))
+	print(min_move('baabab'))
 
 def min_move(s: str)-> int:
-	res, i = 0, 0
-	while i < len(s):
-		next = i+1
-		while next < len(s) and s[i] == s[next]:
-			next += 1
-		res += (next - i)/3
-		i = next
-	return res
-
+	r_count, ans = 0, 0
+	if len(s) == 0: return 0
+	r_char = s[0]
+	for i,c in enumerate(s):
+		if c == r_char:
+			r_count += 1
+			if r_count == 3:
+				r_char = 'a' if c == 'b' else 'b'
+				r_count = 1
+				ans += 1
+		else:
+			r_char = c
+			r_count = 1
+	return ans
+		
 if __name__ == "__main__":
 	main()
